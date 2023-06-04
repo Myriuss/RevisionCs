9c9b61532e5673cb7f149e12c322a8faa8cf3c02
Myriuss <marielelyamani@gmail.com>
2023-06-04 04:37:09 +0100

    :rocket: Woohoo

---
+++ tp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace tp1{
+    class Employe{
+            private int matricule;
+            private float tarifH;
+            private int nbHeures;
+            public double SalaireTotal{get; private set;}
+            public double SalaireDeBase {get; private set;}
+            public string Nom {get; private set;}
+            public int HeuresDeTravail {
+                get => nbHeures;
+                set{
+                    if(value>0){
+                        nbHeures=value;
+                    }
+                    else{
+                        nbHeures=0;
+                    }
+                }
+            }
+        
+    }
+}
